69e3c5cfeb543bbcf1db0ce429fccc4d773e3359
qkrtjdud <qkrtjdudcjst@naver.com>
2023-07-28 11:34:39 +0900

    회원가입DTO

--- src/main/java/shop/mtcoding/blog/controller/UserCotroller.java
+++ src/main/java/shop/mtcoding/blog/controller/UserCotroller.java
@@ -1,12 +1,69 @@
 package shop.mtcoding.blog.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import shop.mtcoding.blog.dto.JoinDTO;
 
 @Controller
 public class UserCotroller {
+
+    // 실무
+    @PostMapping("/join")
+    public String join(JoinDTO joinDTO) {
+        // username=ssar&password=1234&email=ssar@nate.com
+        System.out.println("username : " + joinDTO.getUsername());
+        System.out.println("password : " + joinDTO.getPassword());
+        System.out.println("email : " + joinDTO.getEmail());
+        return "redirect:/loginForm";
+    }
+
+    // // 정상인
+    // @PostMapping("/join")
+    // public String join(String username, String password, String email) {
+    // // username=ssar&password=1234&email=ssar@nate.com
+    // System.out.println("username : " + username);
+    // System.out.println("password : " + password);
+    // System.out.println("email : " + email);
+    // return "redirect:/loginForm";
+    // }
+
+    // // 비정상
+    // @PostMapping("/join")
+    // public String join(HttpServletRequest request) throws IOException {
+    // // username=ssar&password=1234&email=ssar@nate.com
+    // BufferedReader br = request.getReader();
+    // // 버퍼가 소비됨
+    // String body = br.readLine();
+    // // 버퍼에 값이 없어서, 못꺼냄.
+    // String username = request.getParameter("username");
+    // System.out.println("body : " + body);
+    // System.out.println("username : " + username);
+    // return "redirect:/loginForm";
+    // }
+
+    // 약간 정상
+    // DS(컨트롤러 메서드 찾기, 바디데이터 파싱)
+    // DS가 바디데이터를 파싱안하고 컨트롤러 메서드만 찾은 상황
+    // @PostMapping("/join")
+    // public String join(HttpServletRequest request) {
+    // String username = request.getParameter("username");
+    // String password = request.getParameter("password");
+    // String email = request.getParameter("email");
+    // System.out.println("username : " + username);
+    // System.out.println("password : " + password);
+    // System.out.println("email : " + email);
+    // return "redirect:/loginForm";
+    // }
+
     @GetMapping("/joinForm")
-    public String joinFrom() {
+    public String joinForm() {
         return "user/joinForm";
     }
 
